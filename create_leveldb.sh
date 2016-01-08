e97889dad5150da78967978d1b7242c8c38dd7c2
Takuya Wakisaka <takuya.wakisaka@moldweorp.com>
2016-01-08 19:30:40 +0900

    edit leveldb sh

--- create_leveldb.sh
+++ create_leveldb.sh
@@ -9,6 +9,8 @@ rm -rf $DATA/val_leveldb
 convert_imageset \
     -shuffle \
     -backend leveldb \
+    -resize_height 256 \
+    -resize_width 256 \
     $DATA/train/ \
     $DATA/train.txt \
     $DATA/train_leveldb 
@@ -16,9 +18,27 @@ convert_imageset \
 convert_imageset \
     -shuffle \
     -backend leveldb \
+    -resize_height 256 \
+    -resize_width 256 \
     $DATA/val/ \
     $DATA/val.txt \
     $DATA/val_leveldb 
+
+# convert_imageset \
+#     -shuffle \
+#     -resize_height 256 \
+#     -resize_width 256 \
+#     $DATA/train/ \
+#     $DATA/train.txt \
+#     $DATA/train_leveldb 
+
+# convert_imageset \
+#     -shuffle \
+#     -resize_height 256 \
+#     -resize_width 256 \
+#     $DATA/val/ \
+#     $DATA/val.txt \
+#     $DATA/val_leveldb 
 
 
 echo "Done."
